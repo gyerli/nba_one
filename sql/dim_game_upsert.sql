5ad22c283cdfd459fac459b03ccb2c2fbab4260d
Yerli <gyerli@linnenergy.int>
2015-11-24 15:28:51 -0600

    adding sql files

---
+++ sql/dim_game_upsert.sql
@@ -0,0 +1,71 @@
+﻿with new_values 
+as
+(
+SELECT
+  public.hash8(ht.game_id) dim_game_guid,
+  ht.game_id crc_str,
+  ht.game_id id,
+  ht.game_date,
+  replace(ht.season,'-','')::integer season,
+  CASE 
+    WHEN ht.season_type = 'Regular+Season' THEN 'RS'
+    WHEN ht.season_type = 'Playoffs' THEN 'PO'
+    WHEN ht.season_type = 'Pre+Season' THEN 'PS'
+    ELSE 'U'
+  END season_type,  
+  ht.matchup,
+  ht.team_id home_team_id,
+  ht.team_abbreviation home_team_abbrv,
+  rt.team_id road_team_id,
+  rt.team_abbreviation road_team_abbrv,
+  CASE 
+    WHEN ht.wl = 'W' THEN 'H'
+    ELSE 'R'
+  END WL,
+  ht.pts ht_pts,
+  rt.pts rt_pts,
+  ht.min minutes  
+FROM  
+  lnd.vw_games ht
+    INNER JOIN lnd.vw_games rt ON ( ht.game_id = rt.game_id AND
+                                    ht.team_id != rt.team_id AND
+                                    rt.matchup like '%@%')
+),
+upsert as
+(
+UPDATE rpt.dim_game g
+SET 
+  crc_str=nv.crc_str, 
+  id=nv.id, 
+  game_date=nv.game_date, 
+  season=nv.season, 
+  season_type=nv.season_type, 
+  matchup=nv.matchup, 
+  home_team_id=nv.home_team_id, 
+  home_team_abbrv=nv.home_team_abbrv, 
+  road_team_id=nv.road_team_id, 
+  road_team_abbrv=nv.road_team_abbrv, 
+  wl=nv.wl, 
+  ht_pts=nv.ht_pts, 
+  rt_pts=nv.rt_pts, 
+  minutes=nv.minutes, 
+  updated_at=now(),
+  is_active=true
+from new_values nv
+where g.dim_game_guid = nv.dim_game_guid
+returning g.*  
+)
+INSERT INTO rpt.dim_game(
+            dim_game_guid, crc_str, id, game_date, season, season_type, matchup, 
+            home_team_id, home_team_abbrv, road_team_id, road_team_abbrv, 
+            wl, ht_pts, rt_pts, minutes, created_at, updated_at, is_active, 
+            start_date, end_date)
+SELECT dim_game_guid, crc_str, id, game_date, season, season_type, matchup, 
+       home_team_id, home_team_abbrv, road_team_id, road_team_abbrv, 
+       wl, ht_pts, rt_pts, minutes, now(), now(), true, 
+       now(), null
+  FROM new_values
+ where not exists ( select 1 
+                      from upsert up
+                     where up.dim_game_guid = new_values.dim_game_guid )
+;            
---
+++ sql/fct_game_player_upsert.sql
@@ -0,0 +1,51 @@
+﻿SELECT 
+  public.hash8(gp.game_id || '|' || gp.team_id || '|' || gp.player_id)::BIGINT fct_game_player_guid,
+  (gp.game_id || '|' || gp.team_id || '|' || gp.player_id)::varchar crc_str,
+  gp.game_id, 
+  fgt.game_date,
+  gp.team_id, 
+  fgt.team_abbrv,
+  fgt.opp_team_id,
+  fgt.opp_team_abbrv,
+  gp.player_id, 
+  gp.player_name,
+  p.player_name,
+  fgt.matchup,
+  replace(gp.season,'-','')::integer season,
+  CASE 
+    WHEN gp.season_type = 'Regular+Season' THEN 'RS'
+    WHEN gp.season_type = 'Playoffs' THEN 'PO'
+    WHEN gp.season_type = 'Pre+Season' THEN 'PS'
+    ELSE 'U'
+  end::varchar season_type,
+  gp.start_position, 
+  gp.comment cmnt, 
+  gp.min,
+  split_part(gp.min,':',1)::integer * 60 + split_part(gp.min,':',2)::integer seconds,
+  gp.fgm, 
+  gp.fga, 
+  gp.fg_pct,
+  gp.fg3m, 
+  gp.fg3a, 
+  gp.fg3_pct, 
+  gp.ftm, 
+  gp.fta, 
+  gp.ft_pct, 
+  gp.oreb, 
+  gp.dreb, 
+  gp.reb, 
+  gp.ast, 
+  gp.stl, 
+  gp.blk, 
+  gp.tov, 
+  gp.pf, 
+  gp.pts, 
+  gp.plus_minus 
+  FROM lnd.vw_game_player_stat gp
+    left outer join rpt.fct_game_team fgt on ( gp.game_id = fgt.game_id and
+											   gp.team_id = fgt.team_id )
+    left outer join rpt.dim_player p on ( gp.player_id = p.id and 
+									      replace(gp.season,'-','')::integer = p.season)											   
+where gp.game_id = '0021400001'
+  and gp.player_id = 2422
+limit 100  
