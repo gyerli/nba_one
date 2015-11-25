b75f601f69815e02453b6706335817bb731032dc
Yerli <gyerli@linnenergy.int>
2015-11-25 07:54:56 -0600

    dim_player is working

--- sql/archive/dim_player_upsert.sql
+++ sql/archive/dim_player_upsert.sql
@@ -1,31 +1,94 @@
-﻿select 
-  public.hash8(pts.player_id::integer||'|'||replace(pts.season_id,'-','')::integer||'|'||pts.team_id) dim_player_guid,
-  (pts.player_id::integer||'|'||replace(pts.season_id,'-','')::integer||'|'||pts.team_id) crc_str,
-  pts.player_id::integer player_id,
-  coalesce(
-	player, 
-	initcap(split_part(replace(pa.playercode,'HISTADD_',''),'_',1)) || ' ' || initcap(split_part(replace(pa.playercode,'HISTADD_',''),'_',2)) ) player_name,
-  pa.display_last_comma_first,
-  pa.playercode,
-  pts.team_id , 
-  t.team_name team_name, 
-  t.team_abbrv team_abbrv, 
-  p.position pos, 
-  replace(pts.season_id,'-','')::integer season, 
-  (split_part(p.height,'-',1)::integer * 12 + split_part(p.height,'-',2)::integer) height, 
-  p.weight::double precision weight, 
-  p.birth_date dob, 
-  p.age, 
-  p.num jersey_num, 
-  p.exp experience_years, 
-  p.school
-from stg.vw_player_team_season pts
-left outer join lnd.vw_players p on (pts.player_id = p.player_id and 
-									 pts.team_id = p.teamid and
-									 pts.season_id = p.l_season)
-left outer join lnd.vw_teams t on (pts.team_id = t.team_id)									 
-left outer join lnd.vw_players_all pa on (pts.player_id = pa.person_id)
-  
---where pts.player_id=1626204
-order by season_id asc
-
+﻿with new_values as
+(
+select 
+  public.hash8(p.person_id) dim_player_guid,
+  p.person_id crc_str,
+  p.person_id id,
+  p.first_name,
+  p.last_name,
+  p.display_first_last,
+  p.display_last_comma_first,
+  p.display_fi_last,
+  p.birthdate,
+  p.school,
+  p.country,
+  p.last_affiliation,
+  split_part(p.height,'-',1)::integer * 12 + split_part(p.height,'-',2)::integer height,
+  p.weight,
+  p.season_exp,
+  p.jersey,
+  p.position nba_pos,
+  fd.position fd_pos,
+  dk.position dk_pos,
+  p.rosterstatus,
+  p.team_id,
+  p.team_name,
+  p.team_abbreviation team_abbrv,
+  p.team_code,
+  p.team_city,
+  p.playercode,
+  p.from_year,
+  p.to_year,
+  p.dleague_flag,
+  p.games_played_flag
+from lnd.vw_player_info p
+left outer join lnd.vw_fd_players fd on (p.display_first_last = fd.first_name || ' ' || fd.last_name)
+left outer join lnd.vw_dk_players dk on (p.display_first_last = dk.name)
+),
+upsert as
+(
+update rpt.dim_player dp
+   set 
+  crc_str                   = nv.crc_str,
+  id                        = nv.id,
+  first_name                = nv.first_name,
+  last_name                 = nv.last_name,
+  display_first_last        = nv.display_first_last,
+  display_last_comma_first  = nv.display_last_comma_first,
+  display_fi_last           = nv.display_fi_last,
+  birthdate                 = nv.birthdate,
+  school                    = nv.school,
+  country                   = nv.country,
+  last_affiliation          = nv.last_affiliation,
+  height                    = nv.height,
+  weight                    = nv.weight,
+  season_exp                = nv.season_exp,
+  jersey                    = nv.jersey,
+  nba_pos                   = nv.nba_pos,
+  fd_pos                    = nv.fd_pos,
+  dk_pos                    = nv.dk_pos,
+  rosterstatus              = nv.rosterstatus,
+  team_id                   = nv.team_id,
+  team_name                 = nv.team_name,
+  team_abbrv                = nv.team_abbrv,
+  team_code                 = nv.team_code,
+  team_city                 = nv.team_city,
+  playercode                = nv.playercode,
+  from_year                 = nv.from_year,
+  to_year                   = nv.to_year,
+  dleague_flag              = nv.dleague_flag,
+  games_played_flag         = nv.games_played_flag,
+  updated_at 		    = now(),
+  is_active		    = true
+from new_values nv
+where dp.dim_player_guid = nv.dim_player_guid
+returning dp.*
+)
+INSERT INTO rpt.dim_player(
+            dim_player_guid, crc_str, id, first_name, last_name, display_first_last, 
+            display_last_comma_first, display_fi_last, birthdate, school, 
+            country, last_affiliation, height, weight, season_exp, jersey, 
+            nba_pos, fd_pos, dk_pos, rosterstatus, team_id, team_name, team_abbrv, 
+            team_code, team_city, playercode, from_year, to_year, dleague_flag, 
+            games_played_flag, created_at, updated_at, is_active, start_date, 
+            end_date)
+SELECT dim_player_guid, crc_str, id, first_name, last_name, display_first_last, 
+       display_last_comma_first, display_fi_last, birthdate, school, 
+       country, last_affiliation, height, weight, season_exp, jersey, 
+       nba_pos, fd_pos, dk_pos, rosterstatus, team_id, team_name, team_abbrv, 
+       team_code, team_city, playercode, from_year, to_year, dleague_flag, 
+       games_played_flag, now(), now(), true, now(), null
+  FROM new_values
+ where not exists ( select 1
+		      from upsert up
+		     where up.dim_player_guid = new_values.dim_player_guid )
---
+++ sql/rpt/dim_player.sql
@@ -0,0 +1,106 @@
+﻿with new_values as
+(
+select 
+  public.hash8(p.person_id|| '|' || now()) dim_player_guid,
+  p.person_id|| '|' || now() crc_str,
+  p.person_id id,
+  p.first_name,
+  p.last_name,
+  p.display_first_last,
+  p.display_last_comma_first,
+  p.display_fi_last,
+  p.birthdate,
+  p.school,
+  p.country,
+  p.last_affiliation,
+  split_part(p.height,'-',1)::integer * 12 + split_part(p.height,'-',2)::integer height,
+  p.weight,
+  p.season_exp,
+  p.jersey,
+  p.position nba_pos,
+  fd.position fd_pos,
+  dk.position dk_pos,
+  p.rosterstatus,
+  p.team_id,
+  p.team_name,
+  p.team_abbreviation team_abbrv,
+  p.team_code,
+  p.team_city,
+  p.playercode,
+  p.from_year,
+  p.to_year,
+  p.dleague_flag,
+  p.games_played_flag
+from lnd.vw_player_info p 
+left outer join lnd.vw_fd_players fd on (p.display_first_last = fd.first_name || ' ' || fd.last_name)
+left outer join lnd.vw_dk_players dk on (p.display_first_last = dk.name)
+--where person_id = 201571
+),
+update_deactv as
+(
+update rpt.dim_player da
+set 
+  updated_at = now(),
+  end_date	 = now(),
+  is_active	 = false
+from new_values nv
+where da.id = nv.id
+and da.is_active = true
+and (da.first_name <>                 nv.first_name OR 
+	 da.last_name <>                  nv.last_name OR 
+	 da.display_first_last <>         nv.display_first_last OR 
+	 da.display_last_comma_first <>   nv.display_last_comma_first OR 
+	 da.display_fi_last <>            nv.display_fi_last OR 
+	 da.birthdate <>                  nv.birthdate OR 
+	 da.school <>                     nv.school OR 
+	 da.country <>                    nv.country OR 
+	 da.last_affiliation <>           nv.last_affiliation OR 
+	 da.height <>                     nv.height OR 
+	 da.weight <>                     nv.weight OR 
+	 da.season_exp <>                 nv.season_exp OR 
+	 da.jersey <>                     nv.jersey OR 
+	 da.nba_pos <>                    nv.nba_pos OR 
+	 da.fd_pos <>                     nv.fd_pos OR 
+	 da.dk_pos <>                     nv.dk_pos OR 
+	 da.rosterstatus <>               nv.rosterstatus OR 
+	 da.team_id <>                    nv.team_id OR 
+	 da.team_name <>                  nv.team_name OR 
+	 da.team_abbrv <>                 nv.team_abbrv OR 
+	 da.team_code <>                  nv.team_code OR 
+	 da.team_city <>                  nv.team_city OR 
+	 da.playercode <>                 nv.playercode OR 
+	 da.from_year <>                  nv.from_year OR 
+	 da.to_year <>                    nv.to_year OR 
+	 da.dleague_flag <>               nv.dleague_flag OR 
+	 da.games_played_flag <>          nv.games_played_flag)
+returning da.*
+)
+INSERT INTO rpt.dim_player(
+            dim_player_guid, crc_str, id, first_name, last_name, display_first_last, 
+            display_last_comma_first, display_fi_last, birthdate, school, 
+            country, last_affiliation, height, weight, season_exp, jersey, 
+            nba_pos, fd_pos, dk_pos, rosterstatus, team_id, team_name, team_abbrv, 
+            team_code, team_city, playercode, from_year, to_year, dleague_flag, games_played_flag, 
+            created_at, updated_at, is_active, start_date, end_date)
+SELECT dim_player_guid, crc_str, id, first_name, last_name, display_first_last, 
+   display_last_comma_first, display_fi_last, birthdate, school, 
+   country, last_affiliation, height, weight, season_exp, jersey, 
+   nba_pos, fd_pos, dk_pos, rosterstatus, team_id, team_name, team_abbrv, 
+   team_code, team_city, playercode, from_year, to_year, dleague_flag, games_played_flag, 
+   now(), now(), true, now(), null::timestamp
+FROM new_values
+where not exists ( select 1
+		             from rpt.dim_player dp
+		            where dp.id = new_values.id )
+union
+SELECT dim_player_guid, crc_str, id, first_name, last_name, display_first_last, 
+   display_last_comma_first, display_fi_last, birthdate, school, 
+   country, last_affiliation, height, weight, season_exp, jersey, 
+   nba_pos, fd_pos, dk_pos, rosterstatus, team_id, team_name, team_abbrv, 
+   team_code, team_city, playercode, from_year, to_year, dleague_flag, games_played_flag, 
+   now(), now(), true, now(), null::timestamp
+ FROM new_values
+ where exists ( select 1
+		          from update_deactv up
+		         where up.id = new_values.id )
+		     
