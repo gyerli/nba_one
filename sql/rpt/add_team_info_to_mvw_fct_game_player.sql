a04a87c45fc5b14542e8ed8b099881a5f2fdf293
gyerli <gursoy@yerli.org>
2015-12-01 14:57:31 -0600

    mvw update

--- sql/rpt/add_team_info_to_mvw_fct_game_player.sql
+++ sql/rpt/add_team_info_to_mvw_fct_game_player.sql
@@ -13,7 +13,7 @@
 	g.ht_pts,
 	g.rt_pts,
 	g.wl winning_side,
-	dr.*
+	tla.*
 	
    FROM rpt.fct_game_player gp
      left join rpt.fct_game_team gt on ( gp.game_id = gt.game_id and
@@ -22,19 +22,12 @@
 								   g.is_active = true )
 	 LEFT JOIN lnd.vw_player_overall_advanced pa ON gp.player_id = pa.playerid AND gp.season = replace(pa.season::text, '-'::text, ''::text)::integer
 	 LEFT JOIN lnd.vw_player_overall_base pb ON gp.player_id = pb.playerid AND gp.season = replace(pb.season::text, '-'::text, ''::text)::integer
-     LEFT JOIN lnd.vw_player_days_rest_base dr ON (gp.player_id = dr.playerid AND 
-                                                   gp.season = replace(dr.season,'-','')::INTEGER AND 
-                                                   dr.group_value =  
-                                                     CASE
-														WHEN (gp.game_date - a.previous_game_date) = 0 THEN '0 Days Rest'::text
-														WHEN (gp.game_date - a.previous_game_date) = 1 THEN '1 Days Rest'::text
-														WHEN (gp.game_date - a.previous_game_date) = 2 THEN '2 Days Rest'::text
-														WHEN (gp.game_date - a.previous_game_date) = 3 THEN '3 Days Rest'::text
-														WHEN (gp.game_date - a.previous_game_date) = 4 THEN '4 Days Rest'::text
-														WHEN (gp.game_date - a.previous_game_date) = 5 THEN '5 Days Rest'::text
-														WHEN (gp.game_date - a.previous_game_date) > 5 THEN '6+ Days Rest'::text
-														ELSE 'Unknown Days Rest'::text
-													 END )
+	 LEFT JOIN lnd.vw_team_general_location_advanced tla ON (gp.opp_team_id = tla.teamid AND 
+                                                             gp.season = replace(tla.season,'-','')::integer AND 
+                                                             case 
+                                                               when gt.team_loc = 'Home' then 'Road'
+                                                               WHEN gt.team_loc = 'Road' then 'Home'
+                                                             end = tla.team_game_location)
 	 
   WHERE 1=1
   and gp.team_abbrv = 'SAS'
